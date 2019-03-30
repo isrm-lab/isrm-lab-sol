4a8ec3fd804e294160d2e3f403d99ff9e2c66adf
Mihai Barbulescu <b12mihai@gmail.com>
2019-03-30 21:26:10 +0200

    rezolvare colocviu 1

---
+++ col-1/run_udp.sh
@@ -0,0 +1,5 @@
+for n in $(seq 2 1 20); do 
+	echo -n "$n "; 
+	ns  infra.tcl -run_tcp 0 -nn $n  -packetSize 1460 -sendingRate 7M | grep Through | tr '\.' ' ' | awk '$3=="up"{su+=$4} $3=="dn"{sd+=$4} END{print su, sd}'; 
+done | tee col.1.out
+
